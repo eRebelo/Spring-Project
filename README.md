97dbc224c93500f3756001a960321a4eca19c302
eRebelo <eduldrebelo@gmail.com>
2018-10-26 15:05:30 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Java Spring - Maven Project
+  Aplicação que demonstra o uso do framework Spring MVC, Java e Maven, criando uma conexão com banco de dados local Oracle.
+ 
+## Ambiente de Desenvolvimento
+- Eclipse Neon
+- Java 1.8
+- Maven 3.3.9
+- Spring 5.1.RELEASE
+- Tomcat 7
+
+## Execução
+  http://localhost:8080/casadocodigo/produtos/form
---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -0,0 +1,52 @@
+package br.com.casadocodigo.loja.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUTO")
+public class Produto {
+
+	@Id
+	@SequenceGenerator(name = "PRODUTO_ID_GENERATOR", sequenceName = "SEQ_PRODUTO", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRODUTO_ID_GENERATOR")
+	private int id;
+
+	private String titulo;
+	private String descricao;
+	private int paginas;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+
+}
